f476ac8e5b2bc2967cfc6d6dfa81f34441b9d4b2
LaTarisha Fountain <lfountain@njcu.edu>
2018-11-01 00:50:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist_name
+  
   @@all = []
 
   def self.all
@@ -8,6 +9,41 @@ class Song
 
   def save
     self.class.all << self
+  end
+  
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+  
+  def Song.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+  
+  def Song.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+  
+  def Song.find_by_name(song)
+    @@all.find{|element| element.name == song}
+  end
+  
+  def Song.find_or_create_by_name(name)
+    if Song.find_by_name(name)
+      Song.find_by_name(name)
+    else
+      Song.create_by_name(name)
+    end
+  end
+  
+  def Song.alphabetical
+    @@all.sort{|a,b| a.name<=>b.name}
   end
 
 end
